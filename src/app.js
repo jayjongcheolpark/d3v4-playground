588a95886dc2158d5e6e0ec611edb51ee3d5fccd
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-15 23:17:19 -0500

    Add timeScale

--- src/app.js
+++
@@ -1,6 +0,0 @@
-const linearScale = d3.scaleLinear().domain([0, 100]).range([0, 1]).clamp(true)
-
-console.log(linearScale(0))
-console.log(linearScale(50))
-console.log(linearScale(100))
-console.log(linearScale.invert(0.5))
---
+++ src/timeScale.js
@@ -0,0 +1,7 @@
+const timeScale = d3.scaleTime().domain([new Date(2016, 0, 1), new Date()]).range([0, 100])
+
+console.log(timeScale(new Date(2016, 0, 15)))
+console.log(timeScale(new Date(2016, 3, 15)))
+console.log(timeScale(new Date()))
+
+console.log(timeScale.invert(50))
