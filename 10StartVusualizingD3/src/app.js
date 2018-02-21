2cd40776699136be5b47a11c327890387431258e
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-20 22:36:25 -0500

    Data visualization

---
+++ 10StartVusualizingD3/src/app.js
@@ -0,0 +1,26 @@
+const scores = [
+  { name: 'Alice', score: 96 },
+  { name: 'Billy', score: 83 },
+  { name: 'Cindy', score: 91 },
+  { name: 'David', score: 96 },
+  { name: 'Emily', score: 88 },
+]
+
+const update = d3
+  .select('.chart')
+  .selectAll('div')
+  .data(scores, function(d) {
+    return d ? d.name : this.innerText
+  })
+  .style('color', 'blue')
+
+const enter = update.enter().append('div').text(d => d.name).style('color', 'green')
+
+update.exit().remove()
+
+update
+  .merge(enter)
+  .style('width', d => `${d.score}px`)
+  .style('height', '50px')
+  .style('background', 'lightgreen')
+  .style('border', '1px solid black')
